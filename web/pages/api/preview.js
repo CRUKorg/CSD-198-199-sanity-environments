b9f6fd48ba6fc925b01f6a1b79a4f6b62e4bda91
Richard French <richard.french@cancer.org.uk>
2022-03-09 13:21:27 +0000

    updated preview

--- web/pages/api/preview.js
+++ web/pages/api/preview.js
@@ -2,25 +2,28 @@
 
 export default function preview(req, res) {
   if (!req?.query?.secret) {
-    return res.status(401).json({message: 'No secret token'})
+    return res.status(401).json({ message: "No secret token" });
   }
 
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
-  if (req.query.secret !== process.env.SANITY_PREVIEW_SECRET) {
-    return res.status(401).json({message: 'Invalid secret token'})
+  if (
+    req.query.secret !==
+    "sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu	"
+  ) {
+    return res.status(401).json({ message: "Invalid secret token" });
   }
 
   if (!req.query.slug) {
-    return res.status(401).json({message: 'No slug'})
+    return res.status(401).json({ message: "No slug" });
   }
 
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+  res.setPreviewData({});
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, {Location: `/${req?.query?.slug}` ?? `/`})
+  res.writeHead(307, { Location: `/${req?.query?.slug}` ?? `/` });
 
-  return res.end()
+  return res.end();
 }
