43c382e23ad120ef1cf0a971ccb15852211e026c
Richard French <richard.french@cancer.org.uk>
2022-03-11 11:46:49 +0000

    simplify desk setup for race for life

--- studio/desks/raceForLife.js
+++ studio/desks/raceForLife.js
@@ -1,53 +1,18 @@
 import S from "@sanity/desk-tool/structure-builder";
-import {
-  MdComputer,
-  MdContentCopy,
-  MdDashboard,
-  MdDock,
-  MdEdit,
-  MdHomeFilled,
-  MdList,
-  MdStorage,
-  MdMenu,
-  MdMenuOpen,
-  MdTune,
-  MdPerson,
-  MdSettings,
-} from "react-icons/md";
+import { MdComputer } from "react-icons/md";
 const raceForLife = () =>
   S.listItem()
-    .title("Race for Life")
+    .title("Race for life")
     .icon(MdComputer)
     .child(
       S.list()
-        .title("Race for Life")
+        .title("Race for life")
         .items([
-          S.listItem()
-            .title("Homepage")
-            .icon(MdHomeFilled)
-            .child(S.document().schemaType("homepage").documentId("homepage")),
-          S.divider(),
-          S.listItem()
-            .title("Settings")
-            .icon(MdSettings)
-            .child(
-              S.list()
-                .title("Settings Documents")
-                .items([
-                  S.listItem()
-                    .title("Metadata")
-                    .child(
-                      S.document()
-                        .schemaType("siteSettings")
-                        .documentId("siteSettings")
-                    ),
-                  S.listItem()
-                    .title("Site Colors")
-                    .child(
-                      S.document().schemaType("colours").documentId("colours")
-                    ),
-                ])
-            ),
+          ...S.documentTypeListItems().filter(
+            (item) =>
+              item.getId() == "race_for_life_page" ||
+              item.getId() == "race_for_life_section"
+          ),
         ])
     );
 export default raceForLife;
