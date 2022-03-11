f20e3bc41456ce9c4cb1982a65628cfc267cdbbb
Richard French <richard.french@cancer.org.uk>
2022-03-11 11:50:32 +0000

    remove allContent from deskStructure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -3,8 +3,7 @@ import S from "@sanity/desk-tool/structure-builder";
 import mainSite from "./desks/mainSite";
 import raceForLife from "./desks/raceForLife";
 import standUpToCancer from "./desks/standUpToCancer";
-import allContent from "./desks/allContent";
 export default () =>
   S.list()
     .title("Websites")
-    .items([mainSite(), raceForLife(), standUpToCancer(), allContent()]);
+    .items([mainSite(), raceForLife(), standUpToCancer()]);
