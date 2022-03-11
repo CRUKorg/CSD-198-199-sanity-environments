ca436d37bb05f544db6e3850773e8f1df9dfe8b3
Richard French <richard.french@cancer.org.uk>
2022-03-11 11:45:58 +0000

    delete allContent file

--- studio/desks/allContent.js
+++
@@ -1,8 +0,0 @@
-import S from "@sanity/desk-tool/structure-builder";
-import { MdComputer } from "react-icons/md";
-const allContent = () =>
-  S.listItem()
-    .title("ALL CONTENT")
-    .icon(MdComputer)
-    .child(S.list().title("ALL CONTENT").items(S.documentTypeListItems()));
-export default allContent;
