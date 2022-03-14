5cf875dc9674191585c5b0ed6ed5d00b88eacc27
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-14 17:37:54 +0000

    try aws style again

--- main-site/next.config.js
+++ main-site/next.config.js
@@ -1,10 +1,13 @@
 
-module.exports = {
+const nextConfig = {
   /* config options here */
   env: {
     MY_TEST_VARIABLE: "Test 123456789",
     NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+    SANITY_PREVIEW_SECRET: process.env.SANITY_API_TOKEN,
   }
 }
+
+module.exports = nextConfig
