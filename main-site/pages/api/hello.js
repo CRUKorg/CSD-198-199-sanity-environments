b267c086a1f47d374497b8aafe9d1eaa3fc477ab
Richard French <richard.french@cancer.org.uk>
2022-03-14 14:35:36 +0000

    fix typo

--- main-site/pages/api/hello.js
+++ main-site/pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  const t = process.env.MY_TEST_VARIABLE,
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  const t = process.env.MY_TEST_VARIABLE;
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
