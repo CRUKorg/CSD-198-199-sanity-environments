acf074a44299531f99f1d5e5fc4e3b9427463788
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-14 12:13:38 +0000

    multisite resolve preview urls

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,12 +1,23 @@
-const remoteUrl = `https://sanity-poc-delta.vercel.app/`
-const localUrl = `http://localhost:3000`
-
-// const previewSecret = process.env.SANITY_PREVIEW_SECRET
 
 const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
 
-
 export default function resolveProductionUrl(doc) {
+  const localUrl = `http://localhost:3000`
+
+  let  remoteUrl = ''
+
+  switch (doc._type) {
+    case 'main_site_page':
+      remoteUrl = `https://csd-198-199-mainsite.vercel.app/`
+      break
+    case 'race_for_life_page':
+      remoteUrl = `https://csd-198-199-raceforlife.vercel.app/`
+      break
+    case 'stand_up_to_cancer_page':
+      remoteUrl = `https://csd-198-199-standuptocancer.vercel.app/`
+      break
+  }
+
   const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
 
   const previewUrl = new URL(baseUrl)
