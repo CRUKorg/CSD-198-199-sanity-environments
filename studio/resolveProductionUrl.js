b6b4d3d359c20b0d915acf37b7481339f6fa4b03
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-14 10:48:01 +0000

    first pass to resolve prod url

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,7 +1,10 @@
-const remoteUrl = `https://your-nextjs-site.com`
+const remoteUrl = `https://sanity-poc-delta.vercel.app/`
 const localUrl = `http://localhost:3000`
 
-const previewSecret = process.env.SANITY_PREVIEW_SECRET
+// const previewSecret = process.env.SANITY_PREVIEW_SECRET
+
+const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
+
 
 export default function resolveProductionUrl(doc) {
   const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
@@ -10,7 +13,7 @@ export default function resolveProductionUrl(doc) {
 
   previewUrl.pathname = `/api/preview`
   previewUrl.searchParams.append(`secret`, previewSecret)
-  previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`)
+  previewUrl.searchParams.append(`slug`, doc?.uid?.current ?? `/`)
 
   return previewUrl.toString()
 }
