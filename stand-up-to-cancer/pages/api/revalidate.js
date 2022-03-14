d561167bff65a261010d146d162bfd6cffd25423
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-14 10:47:06 +0000

    revalidate on-demand first pass

---
+++ stand-up-to-cancer/pages/api/revalidate.js
@@ -0,0 +1,31 @@
+import { isValidRequest } from '@sanity/webhook'
+
+const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
+
+const revalidate = async (req, res) => {
+
+  if (!isValidRequest(req, previewSecret)) {
+    return res.status(401).json({
+      success: false,
+      message: 'Invalid signature',
+    })
+  }
+
+  const page_uid = req.body.uid.current
+
+  try {
+    // Customise the pages you need to revalidate here.
+    await res.unstable_revalidate(`/${page_uid}`)
+
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error revalidating',
+    })
+  }
+}
+
+export default revalidate
