a2da265e36bdffa670651aa7fcd086dbadb78e16
Richard French <richard.french@cancer.org.uk>
2022-03-14 14:56:41 +0000

    hard code the env vars for now.

--- main-site/next.config.js
+++ main-site/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   /* config options here */
   env: {
     MY_TEST_VARIABLE: "Test 123456789",
+    NEXT_PUBLIC_SANITY_DATASET: "multisite",
+    NEXT_PUBLIC_SANITY_PROJECT_ID: "fiebkckn",
+    SANITY_API_TOKEN:
+      "sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu",
   },
 };
 
