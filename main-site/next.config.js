15654126b3713d289a0ba94f306a758645857323
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-14 16:48:42 +0000

    try aws style

--- main-site/next.config.js
+++ main-site/next.config.js
@@ -1,15 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+
+module.exports = {
   /* config options here */
   env: {
     MY_TEST_VARIABLE: "Test 123456789",
-    NEXT_PUBLIC_SANITY_DATASET: "multisite",
-    NEXT_PUBLIC_SANITY_PROJECT_ID: "fiebkckn",
-    SANITY_API_TOKEN:
-      "sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu",
-  },
-};
-
-module.exports = nextConfig;
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+  }
+}
