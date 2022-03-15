7d1a9b184aa55a2a07b7edcb9fb246dbbeb4f3d0
Richard French <richard.french@cancer.org.uk>
2022-03-15 09:19:13 +0000

    move mainsite preview back to vercel

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,30 +1,31 @@
-
-const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
+const previewSecret =
+  "sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu";
 
 export default function resolveProductionUrl(doc) {
-  const localUrl = `http://localhost:3000`
+  const localUrl = `http://localhost:3000`;
 
-  let  remoteUrl = ''
+  let remoteUrl = "";
 
   switch (doc._type) {
-    case 'main_site_page':
-      remoteUrl = `https://feature-amplify-lambda-env-vars.d3vsky5khu9n46.amplifyapp.com/`
-      break
-    case 'race_for_life_page':
-      remoteUrl = `https://csd-198-199-raceforlife.vercel.app/`
-      break
-    case 'stand_up_to_cancer_page':
-      remoteUrl = `https://csd-198-199-standuptocancer.vercel.app/`
-      break
+    case "main_site_page":
+      remoteUrl = `https://csd-198-199-mainsite.vercel.app/`;
+      break;
+    case "race_for_life_page":
+      remoteUrl = `https://csd-198-199-raceforlife.vercel.app/`;
+      break;
+    case "stand_up_to_cancer_page":
+      remoteUrl = `https://csd-198-199-standuptocancer.vercel.app/`;
+      break;
   }
 
-  const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
+  const baseUrl =
+    window.location.hostname === "localhost" ? localUrl : remoteUrl;
 
-  const previewUrl = new URL(baseUrl)
+  const previewUrl = new URL(baseUrl);
 
-  previewUrl.pathname = `/api/preview`
-  previewUrl.searchParams.append(`secret`, previewSecret)
-  previewUrl.searchParams.append(`slug`, doc?.uid?.current ?? `/`)
+  previewUrl.pathname = `/api/preview`;
+  previewUrl.searchParams.append(`secret`, previewSecret);
+  previewUrl.searchParams.append(`slug`, doc?.uid?.current ?? `/`);
 
-  return previewUrl.toString()
+  return previewUrl.toString();
 }
