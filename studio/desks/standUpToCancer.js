d104f1f9006d3fd27d332fa404d880f7fa76e014
Richard French <richard.french@cancer.org.uk>
2022-03-25 11:26:01 +0000

    use documentTypeList instead.

--- studio/desks/standUpToCancer.js
+++ studio/desks/standUpToCancer.js
@@ -2,12 +2,28 @@ import S from "@sanity/desk-tool/structure-builder";
 import { MdComputer } from "react-icons/md";
 const standUpToCancer = () =>
   S.listItem()
-    .title("SU2C")
-    .schemaType("page")
+    .title("Stand Up To Cancer")
+    .id("standUpToCancer")
+    .icon(MdComputer)
     .child(
-      S.documentList()
-        .title("SU2C Pages")
-        .filter('_type == "page" && site == "standUpToCancer"')
-        .initialValueTemplates([S.initialValueTemplateItem("standUpToCancer")])
+      S.list()
+        .title("Stand Up To Cancer")
+        .id("standUpToCancerChild")
+        .items([
+          S.listItem()
+            .title("All Content")
+            .child(
+              S.documentTypeList("page")
+                .title("All Stand Up To Cancer pages")
+                .filter('site == "standUpToCancer" && _type== "page"')
+            ),
+          S.listItem()
+            .title("Sections")
+            .child(
+              S.documentTypeList("section")
+                .title("All sections")
+                .filter('site == "standUpToCancer" && _type== "section"')
+            ),
+        ])
     );
 export default standUpToCancer;
